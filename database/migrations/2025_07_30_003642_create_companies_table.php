12e08542b50b166dcc1b2cffed85663fcdcc7894
Marcelo Raineri <mraineridev@gmail.com>
2025-08-02 16:11:15 -0300

    feat: remove email field from companies table migration

--- database/migrations/2025_07_30_003642_create_companies_table.php
+++ database/migrations/2025_07_30_003642_create_companies_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('address');
             $table->string('phone');
-            $table->string('email')->unique();
             $table->timestamps();
         });
     }
